5c41b424a8b5bab53e5386b168585ace0efd4fe8
yuriiZherebukh <yura7887@gmail.com>
2017-06-21 18:00:44 +0300

    Added two forms add and change blog article

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')  # queryset
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(Post, pk=pk)  # queryset
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    '''1 param: data from users, 2 param: template
+    where this will be processed, 3d param: global variable for template.'''
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)  # model and primary key for article
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)  # pass this 'post'
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()  # queryset,  saves this 'post'
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
