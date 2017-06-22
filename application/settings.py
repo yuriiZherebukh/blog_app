2d714d75c989b0fa157879f896c02815c6cea404
yuriiZherebukh <yura7887@gmail.com>
2017-06-22 12:06:15 +0300

    Added image field model Post

--- application/settings.py
+++ application/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'application.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS =[ os.path.join(BASE_DIR, 'static'), ]
 
 try:
     from .local_settings import *
--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-            default=timezone.now)
+                   default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    image = models.CharField(max_length=255, default='https://is5-ssl.mzstatic.com/image/thumb/Purple42/v4/d7/7e/e8/d77ee878-f5dd-f1d3-a93a-e19bba1f7183/source/256x256bb.jpg')
 
     def publish(self):
         self.published_date = timezone.now()
